56421ec3cbddf1ce375d6203c52e4c6d952cc8ff
Son Woo Kyu <swk3169@gmail.com>
2020-07-06 18:56:11 +0900

    판매처/현황 지도에 표시

---
+++ mask-map-master/MaskApp/app/src/main/java/com/example/maskapp/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.maskapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.PointF;
+import android.os.Bundle;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.InfoWindow;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.Overlay;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.util.FusedLocationSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, Overlay.OnClickListener, NaverMap.OnCameraChangeListener, NaverMap.OnCameraIdleListener {
+    private static final int ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 100;
+
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+    private InfoWindow infoWindow;
+    private List<Marker> markerList = new ArrayList<Marker>();
+    private boolean isCameraAnimated = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+
+        locationSource = new FusedLocationSource(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE);
+        naverMap.setLocationSource(locationSource);
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(true);
+
+        naverMap.addOnCameraChangeListener((NaverMap.OnCameraChangeListener) this);
+        naverMap.addOnCameraIdleListener((NaverMap.OnCameraIdleListener) this);
+
+        LatLng mapCenter = naverMap.getCameraPosition().target;
+        fetchStoreSale(mapCenter.latitude, mapCenter.longitude, 5000);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case ACCESS_LOCATION_PERMISSION_REQUEST_CODE:
+                locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                return;
+        }
+    }
+
+    private void fetchStoreSale(double lat, double lng, int m) {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://8oi9s0nnth.apigw.ntruss.com").addConverterFactory(GsonConverterFactory.create()).build();
+        MaskApi maskApi = retrofit.create(MaskApi.class);
+        maskApi.getStoresByGeo(lat, lng, m).enqueue(new Callback<StoreSaleResult>() {
+
+            @Override
+            public void onResponse(Call<StoreSaleResult> call, Response<StoreSaleResult> response) {
+                if (response.code() == 200) {
+                    StoreSaleResult result = response.body();
+                    updateMapMarkers(result);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<StoreSaleResult> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void updateMapMarkers(StoreSaleResult result) {
+        resetMarkerList();
+        if (result.stores != null && result.stores.size() > 0) {
+            for (Store store : result.stores) {
+                Marker marker = new Marker();
+                marker.setTag(store);
+                marker.setPosition(new LatLng(store.lat, store.lng));
+                if ("plenty".equalsIgnoreCase(store.remain_stat)) {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_green));
+                } else if ("some".equalsIgnoreCase(store.remain_stat)) {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_yellow));
+                } else if ("fiew".equalsIgnoreCase(store.remain_stat)) {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_red));
+                } else {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_gray));
+                }
+                marker.setAnchor(new PointF(0.5f, 1.0f));
+                marker.setMap(naverMap);
+//                marker.setOnClickListener(this);
+                markerList.add(marker);
+            }
+        }
+    }
+
+    private void resetMarkerList() {
+        if (markerList != null && markerList.size() > 0) {
+            for (Marker marker : markerList) {
+                marker.setMap(null);
+            }
+            markerList.clear();
+        }
+    }
+
+    @Override
+    public boolean onClick(@NonNull Overlay overlay) {
+        if (overlay instanceof Marker) {
+            Marker marker = (Marker) overlay;
+            if (marker.getInfoWindow() != null) {
+                infoWindow.close();
+            } else {
+                infoWindow.open(marker);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onCameraChange(int reason, boolean animated) {
+        isCameraAnimated = animated;
+    }
+
+    @Override
+    public void onCameraIdle() {
+        if (isCameraAnimated) {
+            LatLng mapCenter = naverMap.getCameraPosition().target;
+            fetchStoreSale(mapCenter.latitude, mapCenter.longitude, 5000);
+        }
+    }
+}
---
+++ mask-map-master/MaskApp/app/src/main/java/com/example/maskapp/StoreSaleResult.java
@@ -0,0 +1,8 @@
+package com.example.maskapp;
+
+import java.util.List;
+
+public class StoreSaleResult {
+    public int count;
+    public List<Store> stores;
+}
